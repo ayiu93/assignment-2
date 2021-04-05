8cef257464f4ebd0e89491f3dc79e7465a5e8a26
ayiu93 <anthonyyiu93@gmail.com>
2021-04-04 23:27:49 -0700

    added JS planning for functions

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+/* function to add a new note */
+/*
+    - create a new blank note area when ' + ' button is pressed
+    - make a way to cancel without saving
+*/
+
+
+/* function to select note from side bar*/
+/*
+    - have the side bar populated with title values from note objects in note
+      array
+    - google how to do above
+    - when a side bar button is pressed, searches array for title matching the 
+      button pressed
+    - the associated content from note object is displayed in the text area
+    - probably have to look that up also
+*/
+
+
+/* function to save note */
+/*
+    - ask for name to save note as, is used as title in note object
+    - note and title saved as note object into note array
+    - close note area maybe??
+    - add new title to side bar
+    - will have to look up the whole updating side bar thing
+*/
