f853176490ee20a96ee194a885d8472aba033b6c
ayiu93 <anthonyyiu93@gmail.com>
2021-04-11 20:23:07 -0700

    new notes now show up on side menu

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,12 @@
 const body = document.querySelector("body");
 const mode = document.querySelector(".mode");
 const add = document.querySelector(".add");
-const main = document.querySelector(".main");
 const work = document.querySelector(".workspace");
 const save = document.querySelector(".save");
 const cancel = document.querySelector(".cancel");
-const bar = document.querySelector(".bar");
 
 notesArray = []
+noteCount = 0
 
 mode.onclick = function themeChange() {
 	body.classList.toggle("darkMode");
@@ -41,14 +40,28 @@ function closeNote() {
 closeNote();
 cancel.onclick = closeNote;
 
-save.onclick = function saveNote() {
+save.onclick = function saveNote(noteCount) {
   const note = document.querySelector(".note");
-  const titleText = prompt("Please name your note");
-  notesArray.push({title: titleText, body: note.value})
-  note.remove();
-  save.classList.toggle("hide")
-  cancel.classList.toggle("hide");
-  console.log(notesArray)
+  const titleText = prompt("Please title your note");
+  if (titleText != null) {
+    notesArray.push({title: titleText, body: note.value});
+    changeNav(titleText, noteCount);
+    noteCount++;
+    note.remove();
+    save.classList.toggle("hide");
+    cancel.classList.toggle("hide");
+  }
+}
+
+function changeNav(text, count) {
+  const list = document.querySelector(".list");
+  const newNote = document.createElement("LI");
+  const newNoteA = document.createElement("A");
+  newNoteA.appendChild(document.createTextNode(text));
+  newNoteA.setAttribute("href", "#");
+  newNoteA.title = text;
+  newNote.appendChild(newNoteA)
+  list.appendChild(newNote);
 }
 /* function to add a new note */
 /*
