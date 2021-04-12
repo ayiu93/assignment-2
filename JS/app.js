a77ce50843e43d72210eebbd0d3f9eebdf1dab53
ayiu93 <anthonyyiu93@gmail.com>
2021-04-11 19:13:05 -0700

    working through some stuff

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,56 @@
-const body = document.querySelector("body")
-const mode = document.querySelector(".mode")
-const add = document.querySelector(".add")
-const work = document.querySelector(".workspace")
+const body = document.querySelector("body");
+const mode = document.querySelector(".mode");
+const add = document.querySelector(".add");
+const main = document.querySelector(".main");
+const work = document.querySelector(".workspace");
+const save = document.querySelector(".save");
+const cancel = document.querySelector(".cancel");
+const bar = document.querySelector(".bar");
 
+notesArray = []
 
-function themeChange() {
+mode.onclick = function themeChange() {
 	body.classList.toggle("darkMode");
   if (mode.innerHTML === "Light-theme") {
     mode.innerHTML = "Dark-theme";
   } else {
     mode.innerHTML = "Light-theme";
   }
-  console.log("hit")
 }
 
-mode.onclick = themeChange;
-
-
-function newNote() {
-  work.classList.toggle("hide")
+add.onclick = function newNote() {
+  const newWork = document.createElement("TEXTAREA");
+  newWork.className = "note";
+  newWork.placeholder = "your note here";
+  const newSave = document.createElement("BUTTON");
+  newSave.className = "button save";
+  newSave.innerHTML = "save";
+  const newCancel = document.createElement("BUTTON");
+  newCancel.className = "button cancel";
+  newCancel.innerHTML = "cancel";
+  work.append(newWork);
+  bar.append(newSave);
+  bar.append(newCancel);
+  //save.classList.toggle("hide");
+  //cancel.classList.toggle("hide");
+  refresh();
 }
 
-add.onclick = newNote;
+cancel.onclick = function closeNote() {
+  const note = document.querySelector(".note");
+  const cancel = document.querySelector(".cancel");
+  const save = document.querySelector(".save");
+  note.remove();
+  //
+  //cancel.classList.toggle("hide");
+  save.remove();
+  cancel.remove();
+}
 
+function refresh() {
+  const save = document.querySelector(".save");
+  const cancel = document.querySelector(".cancel");
+}
 /* function to add a new note */
 /*
     - create a new blank note area when ' + ' button is pressed
